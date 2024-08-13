87e5e977169af86aff5559f40327a40638a0d5fd
prithvitewatia <prithvisinghtewatia@gmail.com>
2024-08-13 09:55:22 +0530

    Added models and views

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
+from blog_app_project.blog_app.models import BlogPost
+
 
 # Create your views here.
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-created_on"]
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ["title", "body"]
+    template_name = "blog/post_new.html"
+    success_url = "/blog/"
+    context_object_name = "post"
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    fields = ["title", "body"]
+    template_name = "blog/post_new.html"
+    context_object_name = "post"
+    success_url = "/blog/"
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = "blog/post_delete.html"
+    context_object_name = "post"
+    success_url = "/blog/"

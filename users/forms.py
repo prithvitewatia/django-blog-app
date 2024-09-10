6ba848572a43f3c0e65b436e34841eb9e2c1f30a
prithvitewatia <prithvisinghtewatia@gmail.com>
2024-09-10 21:00:25 +0530

    Added validation for image field in profile

--- users/forms.py
+++ users/forms.py
@@ -22,8 +22,17 @@ class ProfileUpdateForm(forms.ModelForm):
         fields = ['avatar']
 
     def clean_avatar(self):
-        avatar = self.cleaned_data.get('avatar')
-        # TODO complete this method
+        avatar = self.cleaned_data.get('avatar', False)
+
+        if avatar:
+            if avatar.size > 1 * 1024 * 1024:
+                raise forms.ValidationError('Avatar size should be less than 1MB.')
+
+            valid_mime_types = ['image/jpeg', 'image/png']
+
+            if hasattr(avatar, 'content_type') and avatar.content_type not in valid_mime_types:
+                raise forms.ValidationError('Invalid file type, only jpeg or png are allowed')
+
         return avatar
 
 
